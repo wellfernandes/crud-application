8abd7606564034dac064438e944b26b2addbce8d
Welliton Fernandes Leal <wellfernandes@me.com>
2022-04-30 18:48:23 -0300

    test retrieving data from the database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
 
@@ -8,10 +11,32 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = DB.getConnection();
-		System.out.println("Connected to the database.");
-		DB.closeConnection();
-		System.out.println("Disconnected from the database..");
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.createStatement();
+			
+			rs = st.executeQuery("select * from department");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + 
+			    rs.getString("name"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+		
+		//System.out.println("Connected to the database.");
+		//DB.closeConnection();
+		//System.out.println("Disconnected from the database..");
 	}
 
 }
