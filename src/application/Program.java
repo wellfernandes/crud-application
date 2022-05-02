e84f4d359f45268f07edce7b33a19c60ec0996a8
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-02 14:56:38 -0300

    implemented seller dao JDBC class and dao factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,9 +11,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		//Teste class Seller
+		//test class Seller
 		Department department = new Department(1, "development");
 		Seller seller = new Seller(20, "Mike", "mike@devmail.com", new Date(), 5000.0, department);
 		System.out.println(seller); //print test
+		
+		//test class SellerDao
+		SellerDao sellerDao = new DaoFactory().createSellerDao();
+		System.out.println(sellerDao.getClass()); //print test
 	}
 }
