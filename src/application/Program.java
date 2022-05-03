2b04e18454738bec5f2e7a8f8a48335e7a9e8e44
Welliton Fernandes Leal <wellfernandes@me.com>
2022-05-02 21:46:00 -0300

    findById implementation of seller dao JDBC class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,20 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		//test class Seller
-		Department department = new Department(1, "development");
-		Seller seller = new Seller(20, "Mike", "mike@devmail.com", new Date(), 5000.0, department);
-		System.out.println(seller); //print test
-		
+			
 		//test class SellerDao
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
-		System.out.println(sellerDao.getClass()); //print test
+
+		//test SellerDaoJDBC
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller); //test interface sellerDao JDBC
+		
 	}
 }
